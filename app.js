cf13b210ca8e3488325a74ee0bd1f44c08dce8e8
loveAshley <loveashleyu@gmail.com>
2017-04-02 09:13:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 localStorage.CADCNY = 5.1352;
 localStorage.CADUSD = 0.7471;
 $("#convertBtn").on("click", function(e) {
-    var type = $("#type").val();
-    var elem = Number.parseFloat(localStorage[type]) || 0;
-    var inputText = $("#convertTxt").val();
+   const type = $("#type").const();
+   const elem = Number.parseFloat(localStorage[type]) || 0;
+   const inputText = $("#convertTxt").const();
     inputText = Number.parseFloat(inputText);
     $("#result").html(inputText * elem);
 });
